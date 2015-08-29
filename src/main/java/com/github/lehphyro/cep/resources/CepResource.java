ecf51216e1e0749e106df9a8cfb1d4e5a0757049
Leandro <lehphyro@gmail.com>
2015-08-29 13:12:10 -0300

    validando se cep possui somente numeros

--- src/main/java/com/github/lehphyro/cep/resources/CepResource.java
+++ src/main/java/com/github/lehphyro/cep/resources/CepResource.java
@@ -3,19 +3,18 @@ package com.github.lehphyro.cep.resources;
 import com.codahale.metrics.annotation.Timed;
 import com.github.lehphyro.cep.core.Endereco;
 import com.github.lehphyro.cep.service.CepService;
-import com.google.common.base.Charsets;
 import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Digits;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.HttpHeaders;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 @Path("/cep")
-@Produces(MediaType.APPLICATION_JSON)
+@SuppressWarnings("RSReferenceInspection") // charset definido dessa forma é permitido no jersey
+@Produces("application/json; charset=UTF-8")
 public class CepResource {
 
     private final CepService cepService;
@@ -27,11 +26,13 @@ public class CepResource {
     @GET
     @Path("/{numero}")
     @Timed
-    public Response buscarPorNumero(@Length(min = 8, max = 8, message = "CEP inv�lido") @PathParam("numero") String numero) {
+    public Response buscarPorNumero(@Length(min = 8, max = 8, message = "CEP inválido")
+                                    @Digits(integer = 8, fraction = 0, message = "CEP inválido")
+                                    @PathParam("numero") String numero) {
         Endereco endereco = cepService.buscarPorNumero(numero);
         if (endereco == null) {
             return Response.status(Response.Status.NOT_FOUND).build();
         }
-        return Response.ok(endereco).header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_TYPE.withCharset(Charsets.UTF_8.name())).build();
+        return Response.ok(endereco).build();
     }
 }
